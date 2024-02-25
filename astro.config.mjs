5394f7d0d570d3df959d4c5b053473a233ed4c02
Marius PC <mihailmariusion@gmail.com>
2024-02-25 23:06:40 +0100

    update deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 
+// https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   site: "https://mihailmariusiondev.github.io",
 });
